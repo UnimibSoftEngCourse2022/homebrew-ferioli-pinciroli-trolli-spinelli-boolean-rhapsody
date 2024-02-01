c8aad9bff1ebb991851c830fa58d7cf24f592b56
Francesco Trolli <franz.trolli@gmail.com>
2024-02-01 19:10:34 +0100

    Creazione layout base per Fragment Ingredienti

---
+++ app/src/main/java/it/unimib/brewday/IngredientiFragment.java
@@ -0,0 +1,49 @@
+package it.unimib.brewday;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link IngredientiFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class IngredientiFragment extends Fragment {
+
+
+    public IngredientiFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static IngredientiFragment newInstance() {
+
+        return new IngredientiFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ingredienti, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
