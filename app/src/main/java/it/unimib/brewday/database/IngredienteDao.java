96520b090e95a5c8afff72e129acab3d480ddf69
stefanospinelli29 <128166491+stefanospinelli29@users.noreply.github.com>
2024-02-01 12:41:18 +0100

    aggiunto model Ingrediente e Dao Ingrediente

---
+++ app/src/main/java/it/unimib/brewday/database/IngredienteDao.java
@@ -0,0 +1,21 @@
+package it.unimib.brewday.database;
+
+import androidx.room.Dao;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import it.unimib.brewday.model.Ingrediente;
+
+@Dao
+public interface IngredienteDao {
+
+    @Query("SELECT * FROM Ingrediente ")
+    List<Ingrediente> getAll();
+
+    @Update void updateAllIngredienti(List<Ingrediente> Ingredienti);
+
+    @Update int updateIngrediente(Ingrediente ingrediente);
+
+}
--- app/src/main/java/it/unimib/brewday/model/Ingrediente.java
+++ app/src/main/java/it/unimib/brewday/model/Ingrediente.java
@@ -5,9 +5,42 @@ import androidx.room.PrimaryKey;
 @Entity
 public class Ingrediente {
 
-    @PrimaryKey public long id;
+    @PrimaryKey private long id;
+    private String nome;
+
+    private Double quantitàAssoluta;
 
     public Ingrediente() {
         //placeholder
+    }
+
+    public Ingrediente(long id, String nome, Double quantitàAssoluta) {
+        this.id = id;
+        this.nome = nome;
+        this.quantitàAssoluta = quantitàAssoluta;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getQuantitàAssoluta() {
+        return quantitàAssoluta;
+    }
+
+    public void setQuantitàAssoluta(Double quantitàAssoluta) {
+        this.quantitàAssoluta = quantitàAssoluta;
     }
 }
