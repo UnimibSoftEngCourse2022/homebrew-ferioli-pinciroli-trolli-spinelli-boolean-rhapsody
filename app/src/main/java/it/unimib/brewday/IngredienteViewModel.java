117ddc9e8f808f61a829c15e85bb50a0dcd54bcf
stefanospinelli29 <128166491+stefanospinelli29@users.noreply.github.com>
2024-02-01 18:34:49 +0100

    completato IngredienteViewModel

--- app/src/main/java/it/unimib/brewday/IngredienteViewModel.java
+++ app/src/main/java/it/unimib/brewday/IngredienteViewModel.java
@@ -1,15 +1,19 @@
 package it.unimib.brewday;
 
-import androidx.annotation.NonNull;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import java.io.Closeable;
+import java.util.List;
+
+import it.unimib.brewday.model.Ingrediente;
 
 public class IngredienteViewModel extends ViewModel {
     MutableLiveData<Risultato>  readAllIngredientiMutableLiveData;
 
     MutableLiveData<Risultato>  updateIngredientiMutableLiveData ;
+
+    MutableLiveData<Risultato>  updateIngredienteMutableLiveData ;
 
     IngredienteRepository ingredienteRepository;
 
@@ -18,18 +22,25 @@ public class IngredienteViewModel extends ViewModel {
 
         readAllIngredientiMutableLiveData = new MutableLiveData<>();
         updateIngredientiMutableLiveData = new MutableLiveData<>();
+        updateIngredienteMutableLiveData = new MutableLiveData<>();
     }
 
 
 
 
     public void readAllIngredienti(){
-    ingredienteRepository.readAllIngredienti(risultato -> {
+    ingredienteRepository.readAllIngredienti(risultato ->
+            readAllIngredientiMutableLiveData.postValue(risultato));
+    }
 
+    public void UpdateIngrediente(Ingrediente ingrediente){
+        ingredienteRepository.updateIngrediente(ingrediente, risultato ->
+                updateIngredienteMutableLiveData.postValue(risultato));
+    }
 
-            });
-
-
+    public void UpdateIngredienti(List<Ingrediente> listaIngredienti){
+        ingredienteRepository.updateAllIngredienti(listaIngredienti, risultato ->
+                updateIngredienteMutableLiveData.postValue(risultato));
 
     }
 
