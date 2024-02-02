27244ba72a5bc9baa53acc78dca99b1f6e501b71
Luca Pinciroli <l.pinciroli3@campus.unimib.it>
2024-02-02 12:11:01 +0100

    commit di test

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.sonarqube") version "4.4.1.3373"
-    id("jacoco")
 }
 
 sonar {
@@ -10,6 +9,6 @@ sonar {
         property("sonar.projectKey", "UnimibSoftEngCourse2022_homebrew-ferioli-pinciroli-trolli-spinelli-boolean-rhapsody")
         property("sonar.organization", "unimibsoftengcourse2022")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.tests", "src/test,src/androidTest")
+        property("sonar.tests", "src/androidTest")
     }
 }
