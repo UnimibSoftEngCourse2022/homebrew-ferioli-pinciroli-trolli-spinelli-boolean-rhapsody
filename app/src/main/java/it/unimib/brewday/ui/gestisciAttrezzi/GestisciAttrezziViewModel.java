1089c55c422479256d1b3fbf0a702615c74aad1c
Marco Ferioli <m.ferioli@campus.unimib.it>
2024-02-03 12:55:35 +0100

    Refactor dipendenze viewmodel

--- app/src/main/java/it/unimib/brewday/ui/gestisciAttrezzi/GestisciAttrezziViewModel.java
+++ app/src/main/java/it/unimib/brewday/ui/gestisciAttrezzi/GestisciAttrezziViewModel.java
@@ -7,7 +7,6 @@ import androidx.lifecycle.ViewModel;
 
 import it.unimib.brewday.model.Attrezzo;
 import it.unimib.brewday.model.Risultato;
-import it.unimib.brewday.model.TipoAttrezzo;
 import it.unimib.brewday.repository.AttrezziRepository;
 import it.unimib.brewday.util.ServiceLocator;
 
