4a364dfd6a87598c2fda47603941c330636faa3f
Luca Pinciroli <l.pinciroli3@campus.unimib.it>
2024-02-03 12:34:39 +0100

    Fixate alcune issue segnalate da sonar

--- app/src/main/java/it/unimib/brewday/ui/CreaRicettaFragment.java
+++ app/src/main/java/it/unimib/brewday/ui/CreaRicettaFragment.java
@@ -1,9 +1,6 @@
 package it.unimib.brewday.ui;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
