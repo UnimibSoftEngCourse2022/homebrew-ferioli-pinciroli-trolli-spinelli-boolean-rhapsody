494c7a5fcbdf65465a31c8f140f06933f27a23c7
Francesco Trolli <franz.trolli@gmail.com>
2024-02-06 17:13:34 +0100

    Implementato adatper per lista ricette

--- app/src/main/java/it/unimib/brewday/ui/gestisci_ricette/RicetteRecyclerViewAdapter.java
+++ app/src/main/java/it/unimib/brewday/ui/gestisci_ricette/RicetteRecyclerViewAdapter.java
@@ -1,16 +1,24 @@
 package it.unimib.brewday.ui.gestisci_ricette;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import it.unimib.brewday.R;
 
 public class RicetteRecyclerViewAdapter extends
         RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view;
+        view =  LayoutInflater.from(parent.getContext()).inflate(R.layout.elemento_lista_ricetta, parent, false);
+        return new RicettaViewHolder(view);
     }
 
     @Override
@@ -21,5 +29,23 @@ public class RicetteRecyclerViewAdapter extends
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    TextView nomeRicetta;
+    ImageButton aggiungiRicetta;
+
+    public class RicettaViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+
+        public RicettaViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nomeRicetta = itemView.findViewById(R.id.textView_nomeRicetta);
+            aggiungiRicetta = itemView.findViewById(R.id.imageButton_aggiungiRicetta);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
     }
 }
