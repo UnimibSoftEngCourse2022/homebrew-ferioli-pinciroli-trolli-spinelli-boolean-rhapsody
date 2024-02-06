d9c799e2b4e73bca23d834bc0dc5c2fd7ed5a94e
Francesco Trolli <franz.trolli@gmail.com>
2024-02-06 16:24:15 +0100

    Impostata recycclerView per ListaRicetteFragment

--- app/src/main/java/it/unimib/brewday/ui/gestisci_ricette/ListaRicetteFragment.java
+++ app/src/main/java/it/unimib/brewday/ui/gestisci_ricette/ListaRicetteFragment.java
@@ -2,7 +2,11 @@ package it.unimib.brewday.ui.gestisci_ricette;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,5 +32,19 @@ public class ListaRicetteFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lista_ricette, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RecyclerView recyclerViewRicette = view.findViewById(R.id.recyclerview_listaRicette);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(requireContext(),
+                LinearLayoutManager.VERTICAL, false);
+
+        recyclerViewRicette.setLayoutManager(layoutManager);
+        //adapter
+
+
     }
 }
---
+++ app/src/main/java/it/unimib/brewday/ui/gestisci_ricette/RicetteRecyclerViewAdapter.java
@@ -0,0 +1,25 @@
+package it.unimib.brewday.ui.gestisci_ricette;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RicetteRecyclerViewAdapter extends
+        RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
