298eaf6f64e85d9e92de755c13b9a2384b00e24b
Luca Pinciroli <l.pinciroli3@campus.unimib.it>
2024-02-06 19:35:42 +0100

    Inizio implementazione di RicetteViewModel

--- app/src/main/java/it/unimib/brewday/ui/gestisci_ricette/RicetteViewModel.java
+++ app/src/main/java/it/unimib/brewday/ui/gestisci_ricette/RicetteViewModel.java
@@ -1,14 +1,32 @@
 package it.unimib.brewday.ui.gestisci_ricette;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import it.unimib.brewday.model.Risultato;
 import it.unimib.brewday.repository.RicetteRepository;
+import it.unimib.brewday.ui.Callback;
 
 public class RicetteViewModel extends ViewModel {
 
     private final RicetteRepository ricetteRepository;
+    private MutableLiveData<Risultato> readListaRicetteRisultato;
+    private MutableLiveData<Risultato> readRicettaByIdRisultato;
+    private MutableLiveData<Risultato> readRicettaByNameRisultato;
+    private MutableLiveData<Risultato> createRicettaRisultato;
+    private MutableLiveData<Risultato> updateRicettaRisultato;
+    private MutableLiveData<Risultato> deleteRicettaRisultato;
 
     public RicetteViewModel(RicetteRepository ricetteRepository){
         this.ricetteRepository = ricetteRepository;
+    }
+
+    public LiveData<Risultato> readAllRicette(){
+        ricetteRepository.readListaRicette(risultato -> {
+            readListaRicetteRisultato.postValue(risultato);
+        });
+        return readListaRicetteRisultato;
     }
 
 }
