558b55f8ea68d08a677d1315b8cf70e4f75f92d0
Francesco Trolli <franz.trolli@gmail.com>
2024-02-07 11:10:56 +0100

    Modificato CreaRicettaFragment

--- app/src/main/java/it/unimib/brewday/ui/gestisci_ricette/CreaRicettaFragment.java
+++ app/src/main/java/it/unimib/brewday/ui/gestisci_ricette/CreaRicettaFragment.java
@@ -1,13 +1,24 @@
 package it.unimib.brewday.ui.gestisci_ricette;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import it.unimib.brewday.R;
+import it.unimib.brewday.model.Ingrediente;
+import it.unimib.brewday.model.TipoIngrediente;
+import it.unimib.brewday.ui.gestisci_ingredienti.AdapterListViewListaIngredientiDisponibili;
 
 public class CreaRicettaFragment extends Fragment {
 
@@ -26,5 +37,35 @@ public class CreaRicettaFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_crea_ricetta, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ListView listViewIngredientiRicetta = view.findViewById(R.id.listView_ingredrientiRicetta);
+
+        List<Ingrediente> listIngredienti = new ArrayList<>();
+        listIngredienti.add(new Ingrediente(1, TipoIngrediente.ACQUA, 0.0));
+        listIngredienti.add(new Ingrediente(1, TipoIngrediente.ACQUA, 0.0));
+        listIngredienti.add(new Ingrediente(1, TipoIngrediente.ACQUA, 0.0));
+        listIngredienti.add(new Ingrediente(1, TipoIngrediente.ACQUA, 0.0));
+        listIngredienti.add(new Ingrediente(1, TipoIngrediente.ACQUA, 0.0));
+
+        AdapterListViewListaIngredientiDisponibili adapterListViewListaIngredientiRicetta = new AdapterListViewListaIngredientiDisponibili(
+                getContext(), 0, listIngredienti, R.layout.lista_ingredienti_singoli,
+                new AdapterListViewListaIngredientiDisponibili.OnItemClickListener() {
+                    @Override
+                    public void onAddIngredienteClick(Ingrediente ingrediente, int position, EditText quantitaIngrediente) {
+
+                    }
+
+                    @Override
+                    public void onRemoveIngredienteClick(Ingrediente ingrediente, int position, EditText quantitaIngrediente) {
+
+                    }
+                }, (ingrediente, quantitaIngrediente, position) -> {});
+
+        listViewIngredientiRicetta.setAdapter(adapterListViewListaIngredientiRicetta);
+        listViewIngredientiRicetta.setDivider(null);
     }
 }
