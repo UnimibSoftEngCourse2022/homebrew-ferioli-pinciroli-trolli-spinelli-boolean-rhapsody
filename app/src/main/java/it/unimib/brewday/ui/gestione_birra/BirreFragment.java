0912e45a234d6ac1ab38a9a469ba1fcbc4f327be
Marco Ferioli <m.ferioli@campus.unimib.it>
2024-02-11 18:29:24 +0100

    Aggiunta recycler view

--- app/src/main/java/it/unimib/brewday/ui/gestione_birra/BirreFragment.java
+++ app/src/main/java/it/unimib/brewday/ui/gestione_birra/BirreFragment.java
@@ -3,23 +3,36 @@ package it.unimib.brewday.ui.gestione_birra;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import it.unimib.brewday.R;
+import it.unimib.brewday.ui.gestisci_ricette.RicetteViewModel;
+import it.unimib.brewday.ui.gestisci_ricette.RicetteViewModelFactory;
 
 
 public class BirreFragment extends Fragment {
 
 
-    public BirreFragment() {
-    }
+    private BirraViewModel birraViewModel;
 
+    public BirreFragment() {}
 
     public static BirreFragment newInstance() {
         return new BirreFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        birraViewModel = new ViewModelProvider(this,
+                new BirraViewModelFactory(getContext()))
+                .get(BirraViewModel.class);
     }
 
     @Override
