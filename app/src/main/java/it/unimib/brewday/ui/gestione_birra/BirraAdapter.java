849a78900ba5abca582727cc85ac9bb51e265235
Luca Pinciroli <l.pinciroli3@campus.unimib.it>
2024-02-12 11:22:01 +0100

    Creato il layout per la singola birra (istanza di ricetta) nella RecyclerView

---
+++ app/src/main/java/it/unimib/brewday/ui/gestione_birra/BirraAdapter.java
@@ -0,0 +1,22 @@
+package it.unimib.brewday.ui.gestione_birra;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BirraAdapter {
+
+
+
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+
+    }
+}
