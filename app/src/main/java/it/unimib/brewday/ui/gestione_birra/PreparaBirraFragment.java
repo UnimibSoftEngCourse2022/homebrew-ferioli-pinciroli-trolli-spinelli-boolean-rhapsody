90d8aaf1a1ec9e0bac5811e274fed7182533ba9b
Francesco Trolli <franz.trolli@gmail.com>
2024-02-12 12:48:54 +0100

    Modificata navigation

--- app/src/main/java/it/unimib/brewday/ui/gestione_birra/PreparaBirraFragment.java
+++ app/src/main/java/it/unimib/brewday/ui/gestione_birra/PreparaBirraFragment.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,14 +21,18 @@ import it.unimib.brewday.R;
 import it.unimib.brewday.databinding.FragmentPreparaBirraBinding;
 import it.unimib.brewday.model.Ingrediente;
 import it.unimib.brewday.model.IngredienteRicetta;
+import it.unimib.brewday.model.Ricetta;
 import it.unimib.brewday.model.TipoIngrediente;
+import it.unimib.brewday.ui.gestisci_ricette.RicetteViewModel;
+import it.unimib.brewday.ui.gestisci_ricette.RicetteViewModelFactory;
 
 
 public class PreparaBirraFragment extends Fragment {
 
- private FragmentPreparaBirraBinding fragmentPreparaBirraBinding;
+    private FragmentPreparaBirraBinding fragmentPreparaBirraBinding;
+    private RicetteViewModel ricetteViewModel;
 
- private AdapterListViewIngredientiBirra adapterListViewIngredientiBirra;
+    private AdapterListViewIngredientiBirra adapterListViewIngredientiBirra;
     public PreparaBirraFragment() {
         // Required empty public constructor
     }
@@ -38,6 +46,9 @@ public class PreparaBirraFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ricetteViewModel = new ViewModelProvider(this,
+                new RicetteViewModelFactory(getContext()))
+                .get(RicetteViewModel.class);
     }
 
     @Override
@@ -52,15 +63,27 @@ public class PreparaBirraFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         List<IngredienteRicetta> listaIngredientiBirra = new ArrayList<>();
+        Ricetta ricetta = new Ricetta("odod", 1);
 
-        listaIngredientiBirra.add(new IngredienteRicetta(TipoIngrediente.ACQUA, 0.0));
-        listaIngredientiBirra.add(new IngredienteRicetta(TipoIngrediente.ACQUA, 0.0));
-        listaIngredientiBirra.add(new IngredienteRicetta(TipoIngrediente.ACQUA, 0.0));
-        listaIngredientiBirra.add(new IngredienteRicetta(TipoIngrediente.ACQUA, 0.0));
-        listaIngredientiBirra.add(new IngredienteRicetta(TipoIngrediente.ACQUA, 0.0));
-        listaIngredientiBirra.add(new IngredienteRicetta(TipoIngrediente.ACQUA, 0.0));
+        ricetteViewModel.getIngredientiRicetta(ricetta.getId());
+        fragmentPreparaBirraBinding.textViewNomePreparaBirra.setText(ricetta.getNome());
 
-        adapterListViewIngredientiBirra = new AdapterListViewIngredientiBirra(getContext(), R.layout.lista_ingredienti_birra, listaIngredientiBirra);
+        listaIngredientiBirra.add(new IngredienteRicetta(ricetta.getId() ,TipoIngrediente.ACQUA, 0.0));
+        listaIngredientiBirra.add(new IngredienteRicetta(ricetta.getId() ,TipoIngrediente.ADDITIVI, 0.0));
+        listaIngredientiBirra.add(new IngredienteRicetta(ricetta.getId() ,TipoIngrediente.LUPPOLO, 0.0));
+        listaIngredientiBirra.add(new IngredienteRicetta(ricetta.getId() ,TipoIngrediente.MALTO, 0.0));
+        listaIngredientiBirra.add(new IngredienteRicetta(ricetta.getId() ,TipoIngrediente.ZUCCHERO, 0.0));
+        listaIngredientiBirra.add(new IngredienteRicetta(ricetta.getId() ,TipoIngrediente.LIEVITI, 0.0));
+
+        ricetteViewModel.getIngredientiRicetteRisultato().observe(getViewLifecycleOwner(), risultato -> {
+            if (risultato.isSuccessful()){
+                adapterListViewIngredientiBirra = new AdapterListViewIngredientiBirra(getContext(), R.layout.lista_ingredienti_birra, listaIngredientiBirra);
+            } else {
+                Snackbar.make(view, "non riesco a recuperare gli ingredienti", BaseTransientBottomBar.LENGTH_SHORT).show();
+            }
+        });
+
+        //adapterListViewIngredientiBirra = new AdapterListViewIngredientiBirra(getContext(), R.layout.lista_ingredienti_birra, listaIngredientiBirra);
 
     }
 }
