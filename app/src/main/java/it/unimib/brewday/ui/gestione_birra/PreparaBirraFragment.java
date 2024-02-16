b7a358f4e3a61a1a301887fb9ceb9185c0717246
Luca Pinciroli <l.pinciroli3@campus.unimib.it>
2024-02-16 11:29:54 +0100

    Rimossi import inutilizzati (ultima volta)

--- app/src/main/java/it/unimib/brewday/ui/gestione_birra/PreparaBirraFragment.java
+++ app/src/main/java/it/unimib/brewday/ui/gestione_birra/PreparaBirraFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
