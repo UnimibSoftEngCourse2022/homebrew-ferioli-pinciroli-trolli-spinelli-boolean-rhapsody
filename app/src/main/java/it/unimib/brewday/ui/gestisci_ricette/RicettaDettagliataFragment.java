4ac51e77e557746f7fda86d1c6c665d6196035a7
Marco Ferioli <m.ferioli@campus.unimib.it>
2024-02-16 09:56:24 +0100

    Modifiche grafiche su schermata ricetta dettagliata

--- app/src/main/java/it/unimib/brewday/ui/gestisci_ricette/RicettaDettagliataFragment.java
+++ app/src/main/java/it/unimib/brewday/ui/gestisci_ricette/RicettaDettagliataFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -75,6 +77,9 @@ public class RicettaDettagliataFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+
+
         ImageButton modificaRicettaButton = fragmentRicettaDettagliataBinding.imageButtonModificaRicetta;
         EditText numeroLitriRicettaBirra = fragmentRicettaDettagliataBinding.editTextNumberLitriRicettaBirra;
         EditText nomeRicetta = fragmentRicettaDettagliataBinding.textViewNomeRicettaDettagliata;
@@ -85,10 +90,19 @@ public class RicettaDettagliataFragment extends Fragment {
         ricetteViewModel.getIngredientiRicetta(ricetta.getId());
         nomeRicetta.setText(ricetta.getNome());
         numeroLitriRicettaBirra.setText(Integer.toString(ricetta.getLitriDiRiferimento()));
-
         visibile = false;
         numeroLitriRicettaBirra.setEnabled(visibile);
         nomeRicetta.setEnabled(visibile);
+
+        //Gestione titolo Topbar (Deve essere vuoto)
+        if (activity != null) {
+            ActionBar actionBar = activity.getSupportActionBar();
+
+            // Imposta il titolo personalizzato
+            if (actionBar != null) {
+                actionBar.setTitle("");
+            }
+        }
 
         ricetteViewModel.getIngredientiRicetteRisultato().observe(getViewLifecycleOwner(), risultato -> {
             if (risultato.isSuccessful()){
