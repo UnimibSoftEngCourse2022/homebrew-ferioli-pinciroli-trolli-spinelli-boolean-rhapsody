f702caf6f939cc42e6f3796f0366a45976ceb4a6
Luca Pinciroli <l.pinciroli3@campus.unimib.it>
2024-02-19 10:37:17 +0100

    Refactoring issue di sonar

--- app/src/main/java/it/unimib/brewday/database/BirraDao.java
+++ app/src/main/java/it/unimib/brewday/database/BirraDao.java
@@ -1,7 +1,6 @@
 package it.unimib.brewday.database;
 
 import androidx.room.Dao;
-import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
--- app/src/main/java/it/unimib/brewday/ui/gestione_birra/BirraDettagliataFragment.java
+++ app/src/main/java/it/unimib/brewday/ui/gestione_birra/BirraDettagliataFragment.java
@@ -24,19 +24,9 @@ public class BirraDettagliataFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_birra_dettagliata, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
     }
 }
